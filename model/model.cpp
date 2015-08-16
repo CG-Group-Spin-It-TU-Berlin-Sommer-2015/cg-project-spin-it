9e47755c12e9f0e9fae07f3e05327a5674fb2fe7
felixvoelker <felix.voelker@campus.tu-berlin.de>
2015-08-16 21:55:01 +0200

    SQP Optimization Method + Powell strategy for strict hollowing integrated.

--- model/model.cpp
+++ model/model.cpp
@@ -3,8 +3,11 @@
 float Model::p = 1.00;
 QVector3D Model::cp;
 
+float Model::w_c = 1;
+float Model::w_I = 1;
+
 Mesh* Model::mesh = 0;
-float* Model::mesh_volumne = 0;
+float* Model::mesh_volume = 0;
 
 
 void Model::initialize(Mesh* mesh)
@@ -15,21 +18,60 @@ void Model::initialize(Mesh* mesh)
 
 void Model::hollow()
 {
-    mesh_volumne = calculateVolumne(Model::mesh, p);
+    QVector<float>* geometry = Model::mesh->getGeometry();
+    mesh_volume = calculateVolume(Model::mesh, p);
     cout << "Volumnes:" << endl;
     for (int i = 0; i < 10; i++) {
-        cout << mesh_volumne[i] << endl;
+        cout << mesh_volume[i] << endl;
     }
-    Model::spinability(0,0,mesh_volumne);
+
+    //move center to (0,0,0)
+    QVector3D c;
+    c.setX(1/mesh_volume[0]*mesh_volume[1]);
+    c.setY(1/mesh_volume[0]*mesh_volume[2]);
+    c.setZ(1/mesh_volume[0]*mesh_volume[3]);
+
+    cout << "Center of Mass: (" << c.x() << "," << c.y() << "," << c.z() << ")" << endl;
+
+    QVector<GLfloat>* tmp = new QVector<GLfloat>();
+    for (int i = 0; i < geometry->size(); i++) {
+        tmp->push_back(0);
+    }
+
+    Mesh* working_copy = new Mesh(tmp, Model::mesh->getSurfaceNormals(), Model::mesh->getVertexNormals(), Model::mesh->getIndices());
+    for (int i = 0; i < geometry->size(); i += 3) {
+            working_copy->getGeometry()->replace(i, geometry->at(i) - cp.x());
+            working_copy->getGeometry()->replace(i + 1, geometry->at(i + 1) - 0);
+            working_copy->getGeometry()->replace(i + 2, geometry->at(i + 2) - 0);
+    }
+
+    mesh_volume = calculateVolume(working_copy, p);
+    cout << "Volumnes:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << mesh_volume[i] << endl;
+    }
+
+    c.setX(1/mesh_volume[0]*mesh_volume[1]);
+    c.setY(1/mesh_volume[0]*mesh_volume[2]);
+    c.setZ(1/mesh_volume[0]*mesh_volume[3]);
+
+    cout << "Center of Mass: (" << c.x() << "," << c.y() << "," << c.z() << ")" << endl;
+    cout << "Done" << endl;
+    VectorXf b(3);
+    b(0) = 1;
+    b(1) = 1;
+    b(2) = 1;
+    MatrixXf S(3,10);
+    optimize(b,S);
 }
 
 /**
- * @brief Model::calculateVolumne calculates volumne integrals of an object
+ * @brief Model::calculateVolume calculates volumne integrals of an object
  * @param mesh triangulated surface of object
  * @param p density of object
  * @return
  */
-float* Model::calculateVolumne(Mesh* mesh, float p)
+float* Model::calculateVolume(Mesh* mesh, float p)
 {
     float* s = new float[10];
     for (int i = 0; i < 10; i++) {
@@ -104,38 +146,437 @@ float* Model::calculateVolumne(Mesh* mesh, float p)
 }
 
 /**
- * @brief Model::slqp Sequentiel Linear quadratic programming
- * @param b startvector
- * @return
+ * @brief Model::eqMatrix Sets up the block matrix A of equality constraints Ax = b for quadratic auxilary problem
+ * @param b current beta distribution
+ * @param S internal mass distribution
+ * @return H
  */
-float *Model::slqp(float *b)
+MatrixXf Model::eqMatrix(VectorXf b, MatrixXf S)
 {
-
+    MatrixXf H(5, b.rows());
+    H.row(0) = S.col(1);
+    H.row(1) = S.col(2);
+    H.row(2) = S.col(4);
+    H.row(3) = S.col(5);
+    H.row(4) = S.col(6);
+    return H;
 }
 
 /**
- * @brief Model::spinability measures the spinability of an object
- * @param w_i weight of yoyo optimization
- * @param w_c weight of top optimization
- * @param volume volume integrals
- * @return value of objective function
+ * @brief Model::eqVector Sets up the vector b of equality constraints Ax = b for quadratic auxilary problem
+ * @param b current beta distribution
+ * @param S internal mass distribution
+ * @return h
  */
-float Model::spinability(float w_i, float w_c, float* volume)
+VectorXf Model::eqVector(VectorXf b, MatrixXf S)
 {
-    float M = volume[0];
-
-    QVector3D c;
-    c.setX(1/M*volume[1]);
-    c.setY(1/M*volume[2]);
-    c.setZ(1/M*volume[3]);
-
-    float I[3][3] = {{volume[8] + volume[9], -volume[4], -volume[6]},
-                   {-volume[4], volume[7] + volume[9], -volume[5]},
-                   {-volume[6], -volume[5], volume[7] + volume[8]}};
-
-    float f_yoyo = w_i * ((I[1][1]/I[3][3])*(I[1][1]/I[3][3]) + (I[2][2]/I[3][3])*(I[2][2]/I[3][3]));
-    float f_top  = w_c * ((c-cp).length()*M)*((c-cp).length()*M) + f_yoyo;
-
-    return f_top;
+    VectorXf h(5);
+    h(0) = Matrix<float,1,1>(Model::mesh_volume[1]) - b.transpose() * S.col(1);
+    h(1) = Matrix<float,1,1>(Model::mesh_volume[2]) - b.transpose() * S.col(2);
+    h(2) = Matrix<float,1,1>(Model::mesh_volume[4]) - b.transpose() * S.col(4);
+    h(3) = Matrix<float,1,1>(Model::mesh_volume[5]) - b.transpose() * S.col(5);
+    h(4) = Matrix<float,1,1>(Model::mesh_volume[6]) - b.transpose() * S.col(6);
+    return h;
 }
 
+/**
+ * @brief Model::ineqMatrix Sets up the block matrix G of inequality constraints Gx <= r for quadratic auxilary problem
+ * @param b current beta distribution
+ * @param J index set of active inequality constraints
+ * @return G
+ */
+MatrixXf Model::ineqMatrix(VectorXf b, QVector<int> J)
+{
+    VectorXf zero = VectorXf::Zero(b.rows());
+    MatrixXf G(J.size(), b.rows());
+    for (int i = 0; i < J.size(); i++) {
+        zero(J.at(i) / 2) = 1;
+        G.row(i) = zero;
+        zero(J.at(i) / 2) = 0;
+    }
+    return G;
+}
+
+/**
+ * @brief Model::ineqVector Sets up the vector r of inequality constraints Gx <= r for quadratic auxilary problem
+ * @param b current beta distribution
+ * @param J index set of active inequality constraints
+ * @return g
+ */
+VectorXf Model::ineqVector(VectorXf b, QVector<int> J)
+{
+    VectorXf g(J.size());
+    for (int i = 0; i < J.size(); i++) {
+        if (J.at(i) % 2 == 0) {
+            g(i) = -b(J.at(i) / 2);
+        }
+        if (J.at(i) % 2 == 1) {
+            g(i) = 1 - b(J.at(i) / 2);
+        }
+    }
+
+    return g;
+}
+
+/**
+ * @brief Model::gradient Calculates the gradient of objective function f
+ * @param b current beta distribution
+ * @param S internal mass distribution
+ * @return grad
+ */
+VectorXf Model::gradient(VectorXf b, MatrixXf S)
+{
+    VectorXf u1 = Matrix<float,1,1>(Model::mesh_volume[8]) - b.transpose() * S.col(8) + Matrix<float,1,1>(Model::mesh_volume[9]) - b.transpose() * S.col(9);
+    VectorXf u2 = Matrix<float,1,1>(Model::mesh_volume[7]) - b.transpose() * S.col(7) + Matrix<float,1,1>(Model::mesh_volume[9]) - b.transpose() * S.col(9);
+    VectorXf v = Matrix<float,1,1>(Model::mesh_volume[7]) - b.transpose() * S.col(7) + Matrix<float,1,1>(Model::mesh_volume[8]) - b.transpose() * S.col(8);
+    VectorXf grad_top = -2 * Model::w_c * (Matrix<float,1,1>(Model::mesh_volume[3]) - b.transpose() * S.col(3)) * S.col(3);
+    VectorXf grad_yoyo = -2 * Model::w_I * (((S.col(8) + S.col(9))*u1*v + (S.col(7) + S.col(9))*u2*v - (u1*u1 + u2*u2)*(S.col(7)+S.col(8))) / (v(0) * v(0) * v(0)));
+    VectorXf grad = grad_top + grad_yoyo;
+    cout << grad << endl << endl;
+    return grad;
+}
+
+/**
+ * @brief Model::optimize Active set SQP strategy to optimize b
+ * @param b start distribution
+ * @param S internal mass distribution
+ * @return b optimized distribution
+ */
+VectorXf Model::optimize(VectorXf b, MatrixXf S)
+{
+    DiagonalMatrix<float, Dynamic, Dynamic> A;
+    A.resize(b.rows());
+    A.setIdentity();
+
+    VectorXf d = VectorXf::Ones();
+    VectorXf lamda = -VectorXf::Ones();
+    VectorXf mu = -VectorXf::Ones();
+
+    float alpha = 0.1;
+    float gamma = 0.01;
+
+    while (!(d == 0 && mu >= 0)) {
+        grad = gradient(b,S);
+
+        //Setting up equality constraints
+        H = eqMatrix(b, S);
+        h = eqVector(b, S);
+
+        //Setting up active inequality constraints
+        QVector<int> J;
+        for (int i = 0; i < 2*b.rows(); i++) {
+            if (i % 2 == 0) {
+                if (b(i / 2) == 0) {
+                    J.push_back(i);
+                }
+            }
+            if (i % 2 == 1) {
+                if (b(i / 2) == 1) {
+                    J.push_back(i);
+                }
+            }
+        }
+
+        G = ineqMatrix(b, J);
+        g = ineqVector(b, J);
+
+        MatrixXf L(A.rows() + H.rows() + G.rows(), A.cols() + H.cols() + G.cols());
+        L.block(0, 0, A.rows(), A.cols()) = A;
+        L.block(A.rows() + 1, 0, H.rows(), H.cols()) = H;
+        L.block(0, A.cols() + 1, H.cols(), H.rows()) = H.transpose();
+        L.block(A.rows() + H.rows() + 1, 0, G.rows(), G.cols()) = G;
+        L.block(0, A.cols() + H.cols() + 1, G.cols(), G.rows()) = G.transpose();
+        L.block(A.rows() + 1, A.cols() + 1, H.rows() + G.rows(), H.rows() + G.rows()).setZero();
+
+        VectorXf c(grad.rows() + h.rows() + g.rows());
+        c.block(0 , 0, grad.rows(), 1) = -grad;
+        c.block(grad.rows() + 1, 0, h.rows(), 1) = h;
+        c.block(grad.rows() + h.rows() + 1, 0, g.rows(), 1) = g;
+
+        VectorXf x = L.fullPivHouseholderQr().solve(c);
+        d = x.block(0, 0, b.rows(), 1);
+        lamda = x.block(b.rows() + 1, 0, h.rows(), 1);
+        mu = x.block(b.rows() + h.rows() + 1, 0, g.rows(), 1);
+        delete L;
+        delete c;
+        delete x;
+
+        if (d == 0 && !(mu >= 0)) {
+            //Inactivity step
+            int j = 0;
+            float lowest = mu(0);
+            for (int i = 1; i < mu.rows(); i++) {
+                if (mu(i) < mu(j)) {
+                    j = i;
+                    lowest = mu(i);
+                }
+            }
+            J.removeAt(j);
+
+            G = ineqMatrix(b, J);
+            g = ineqVector(b, J);
+
+            L(A.rows() + H.rows() + G.rows(), A.cols() + H.cols() + G.cols());
+            L.block(0, 0, A.rows(), A.cols()) = A;
+            L.block(A.rows() + 1, 0, H.rows(), H.cols()) = H;
+            L.block(0, A.cols() + 1, H.cols(), H.rows()) = H.transpose();
+            L.block(A.rows() + H.rows() + 1, 0, G.rows(), G.cols()) = G;
+            L.block(0, A.cols() + H.cols() + 1, G.cols(), G.rows()) = G.transpose();
+            L.block(A.rows() + 1, A.cols() + 1, H.rows() + G.rows(), H.rows() + G.rows()).setZero();
+
+            c(grad.rows() + h.rows() + g.rows());
+            c.block(0 , 0, grad.rows(), 1) = -grad;
+            c.block(grad.rows() + 1, 0, h.rows(), 1) = h;
+            c.block(grad.rows() + h.rows() + 1, 0, g.rows(), 1) = g;
+
+            x = L.fullPivHouseholderQr().solve(c);
+            d = x.block(0, 0, b.rows(), 1);
+            delete L;
+            delete c;
+            delete x;
+        }
+
+
+        //Powell step-size with merit function
+        for (int i = 0; i < lambda.rows(); i++) {
+            if (lambda(i) + gamma > alpha) {
+                alpha = lambda(i) + gamma;
+            }
+        }
+        for (int i = 0; i < mu.rows(); i++) {
+            if (mu(i) + gamma > alpha) {
+                alpha = mu(i) + gamma;
+            }
+        }
+
+        float sigma = powell(b, S, d, alpha, 0.25, 0.75);
+
+        //BFGS Update
+        VectorXf s = (b + sigma * d) - b;
+        VectorXf y = gradient(b + sigma * d, S) - grad;
+        A = A - ((A*s)*(A*s).transpose())/(s.transpose()*A*s)(0,0) + y*y.transpose()/(y.dot(s))(0,0);
+
+        b = b + sigma * d;
+    }
+
+
+    return b;
+}
+
+/**
+ * @brief Model::powell Calculates efficient step-size with powell strategy
+ * @param b current beta distribution
+ * @param S internal mass distribution
+ * @param d descend direction
+ * @param alpha scaling factor for merit function
+ * @param delta bias for powell step-size
+ * @param beta bias for powell step-size
+ * @return sigma
+ */
+float Model::powell(VectorXf b, MatrixXf S, VectorXf d, float alpha, float delta, float beta)
+{
+    float sigma = 1;
+    float a1 = sigma;
+    float a2 = sigma;
+
+    float g1 = powellG1(sigma, b, S, d, alpha);
+    float g2 = powellG2(sigma, b, S, d, alpha);
+
+    if (g1 >= delta && g2 <= beta) {
+        return sigma;
+    }
+    if (g1 >= delta && g2 > beta) {
+        a1 = sigma;
+        int l = 1;
+        while (!(powellG1(a2, b, S, d, alpha) < delta)) {
+            a2 = pow(2, l) * sigma;
+            l += 1;
+        }
+    } else {
+        if (g1 < delta && g2 <= beta) {
+            a2 = sigma;
+            int l = 1;
+            while (!(powellG1(a1, b, S, d, alpha) >= delta && powellG2(a1, b, S, d, alpha) > beta)) {
+                a1 = pow(2, -l) * sigma;
+                l += 1;
+            }
+        }
+    }
+
+    while (!(g1 >= delta && g2 <= beta)) {
+        sigma = 0.5 * (a1 + a2);
+
+        g1 = powellG1(sigma, b, S, d, alpha);
+        g2 = powellG2(sigma, b, S, d, alpha);
+
+        if (g1 >= delta && g2 > beta) {
+            a1 = sigma;
+        } else {
+             if (g1 < delta && g2 <= beta) {
+                 a2 = sigma;
+             }
+        }
+    }
+
+    return sigma;
+}
+
+/**
+ * @brief Model::powellG1 Auxilary function for powell strategy. Calculates g1(x).
+ * @param sigma current step-size
+ * @param b current beta distribution
+ * @param S internal mass distribution
+ * @param d descend direction
+ * @param alpha scaling factor for merit function
+ * @return g
+ */
+float Model::powellG1(float sigma, VectorXf b, MatrixXf S, VectorXf d, float alpha)
+{
+    float g = 1;
+    if (sigma > 0) {
+        g = (penalty(b + sigma * d, S, alpha) - penalty(b, S, alpha)) / (sigma * penaltyDirectionalGradient(b, S, d, alpha));
+    }
+    return g;
+}
+
+/**
+ * @brief Model::powellG1 Auxilary function for powell strategy. Calculates g2(x).
+ * @param sigma current step-size
+ * @param b current beta distribution
+ * @param S internal mass distribution
+ * @param d descend direction
+ * @param alpha scaling factor for merit function
+ * @return
+ */
+float Model::powellG2(float sigma, VectorXf b, MatrixXf S, VectorXf d, float alpha)
+{
+    return penaltyDirectionalGradient(b + sigma * d, S, d, alpha) / penaltyDirectionalGradient(b, S, d, alpha);
+}
+
+/**
+ * @brief Model::penalty Returns value of merit function.
+ * @param b current beta distribution
+ * @param S internal mass distribution
+ * @param alpha scaling factor for merit function
+ * @return
+ */
+float Model::penalty(VectorXf b, MatrixXf S, float alpha)
+{
+    float Ix = (Matrix<float,1,1>(Model::mesh_volume[8]) - b.transpose() * S.col(8)) + (Matrix<float,1,1>(Model::mesh_volume[9]) - b.transpose() * S.col(9));
+    float Iy = (Matrix<float,1,1>(Model::mesh_volume[7]) - b.transpose() * S.col(7)) + (Matrix<float,1,1>(Model::mesh_volume[9]) - b.transpose() * S.col(9));
+    float Iz = (Matrix<float,1,1>(Model::mesh_volume[7]) - b.transpose() * S.col(7)) + (Matrix<float,1,1>(Model::mesh_volume[8]) - b.transpose() * S.col(8));
+    float s1 = (Matrix<float,1,1>(Model::mesh_volume[3]) - b.transpose() * S.col(3)) * (Matrix<float,1,1>(Model::mesh_volume[3]) - b.transpose() * S.col(3));
+    float s2 = (Ix * Ix) / (Iz * Iz) + (Iy * Iy) / (Iz * Iz);
+
+    VectorXf h(5);
+    h(0) = abs(Matrix<float,1,1>(Model::mesh_volume[1]) - b.transpose() * S.col(1));
+    h(1) = abs(Matrix<float,1,1>(Model::mesh_volume[2]) - b.transpose() * S.col(2));
+    h(2) = abs(Matrix<float,1,1>(Model::mesh_volume[4]) - b.transpose() * S.col(4));
+    h(3) = abs(Matrix<float,1,1>(Model::mesh_volume[5]) - b.transpose() * S.col(5));
+    h(4) = abs(Matrix<float,1,1>(Model::mesh_volume[6]) - b.transpose() * S.col(6));
+
+    VectorXf g(2 * b.rows());
+    for (int i = 0; i < g.rows(); i++) {
+        if (i % 2 == 0) {
+            g(i) = (-b(i/2) + abs(-b(i/2))) / 2;
+
+        }
+        if (i % 2 == 1) {
+            g(i) = ((b(i/2) - 1) + abs(b(i/2) - 1)) / 2;
+        }
+    }
+
+    VectorXf ones = VectorXf.Ones(h.rows());
+    return Model::w_c * s1 + Model::w_I * s2 + alpha * (ones.transpose() * h) + alpha * (ones.transpose * g);
+}
+
+/**
+ * @brief Model::penaltyDirectionalGradient Returns value of directional derivate of merit function in direction d.
+ * @param b current beta distribution
+ * @param S internal mass distribution
+ * @param d descent direction
+ * @param alpha scaling factor for merit function
+ * @return
+ */
+float Model::penaltyDirectionalGradient(VectorXf b, MatrixXf S,  VectorXf d, float alpha)
+{
+    float result = (gradient(b,S).transpose() * d)(0);
+
+    float h = Matrix<float,1,1>(Model::mesh_volume[1]) - b.transpose() * S.col(1);
+    if (h > 0) {
+       result += alpha * (-S(1).transpose() * d);
+    } else {
+        if (h < 0) {
+            result -= alpha * (-S(1).transpose() * d);
+        } else {
+            if (h == 0) {
+                result += alpha * (abs(-S(1).transpose() * d));
+            }
+        }
+    }
+    h = Matrix<float,1,1>(Model::mesh_volume[2]) - b.transpose() * S.col(2);
+    if (h > 0) {
+       result += alpha * (-S(2).transpose() * d);
+    } else {
+        if (h < 0) {
+            result -= alpha * (-S(2).transpose() * d);
+        } else {
+            if (h == 0) {
+                result += alpha * (abs(-S(2).transpose() * d));
+            }
+        }
+    }
+    h = Matrix<float,1,1>(Model::mesh_volume[4]) - b.transpose() * S.col(4);
+    if (h > 0) {
+       result += alpha * (-S(4).transpose() * d);
+    } else {
+        if (h < 0) {
+            result -= alpha * (-S(4).transpose() * d);
+        } else {
+            if (h == 0) {
+                result += alpha * (abs(-S(4).transpose() * d));
+            }
+        }
+    }
+    h = Matrix<float,1,1>(Model::mesh_volume[5]) - b.transpose() * S.col(5);
+    if (h > 0) {
+       result += alpha * (-S(5).transpose() * d);
+    } else {
+        if (h < 0) {
+            result -= alpha * (-S(5).transpose() * d);
+        } else {
+            if (h == 0) {
+                result += alpha * (abs(-S(5).transpose() * d));
+            }
+        }
+    }
+    h = Matrix<float,1,1>(Model::mesh_volume[6]) - b.transpose() * S.col(6);
+    if (h > 0) {
+       result += alpha * (-S(6).transpose() * d);
+    } else {
+        if (h < 0) {
+            result -= alpha * (-S(6).transpose() * d);
+        } else {
+            if (h == 0) {
+                result += alpha * (abs(-S(6).transpose() * d));
+            }
+        }
+    }
+
+    float g = 0;
+    for (int i = 0; i < 2 * b.rows(); i++) {
+        if (i % 2 == 0) {
+            g = -b(i / 2);
+            if (g > 0 || (g == 0 && -d(i/2) > 0)) {
+                result += alpha * -d(i/2);
+            }
+        } else {
+            g = b(i / 2) - 1;
+            if (g > 0 || (g == 0 && d(i/2) > 0)) {
+                result += alpha * d(i/2);
+            }
+        }
+    }
+
+    return result;
+}
--- model/model.h
+++ model/model.h
@@ -2,9 +2,11 @@
 #define MODEL_H
 
 #include <stdio.h>
+#include <Eigen/Dense>
 
 #include "view/utility/mesh.h"
 
+using namespace Eigen;
 using namespace std;
 
 class Model
@@ -13,18 +15,30 @@ public:
     static float p;
     static QVector3D cp;
 
+    static float w_c;
+    static float w_I;
+
 private:
     static Mesh* mesh;
-    static float* mesh_volumne;
+    static float* mesh_volume;
 
 public:
     static void initialize(Mesh *mesh);
     static void hollow();
 
 private:
-    static float* calculateVolumne(Mesh* mesh, float p);
-    static float* slqp(float* b);
-    static float spinability(float w_1, float w_2, float* volume);
+    static float* calculateVolume(Mesh* mesh, float p);
+    static MatrixXf eqMatrix(VectorXf b, MatrixXf S);
+    static VectorXf eqVector(VectorXf b, MatrixXf S);
+    static MatrixXf ineqMatrix(VectorXf b, QVector<int> J);
+    static VectorXf ineqVector(VectorXf b, QVector<int> J);
+    static VectorXf gradient(VectorXf b, MatrixXf S);
+    static VectorXf optimize(VectorXf b, MatrixXf S);
+    static float powell(VectorXf b, MatrixXf S, VectorXf d, float alpha, float delta, float beta);
+    static float powellG1(float sigma, VectorXf b, MatrixXf S, VectorXf d, float alpha);
+    static float powellG2(float sigma, VectorXf b, MatrixXf S, VectorXf d, float alpha);
+    static float penalty(VectorXf b, MatrixXf S, float alpha);
+    static float penaltyDirectionalGradient(VectorXf b, MatrixXf S, VectorXf d, float alpha);
 };
 
 #endif // MODEL_H
