ec73be555c53c810b0e832f306e00077aa53ed1e
sellmann-s <sascha989@gmx.net>
2015-08-31 23:39:06 +0200

    - ignore Cork Boolena Library

--- view/utility/meshbooleanfunctions.cpp
+++ view/utility/meshbooleanfunctions.cpp
@@ -3,6 +3,7 @@
 Mesh* booleanHelper(Mesh* inputmesh1, Mesh* inputmesh2,GLint type)
 {
 
+    /*
     int length = 0;
 
     // set cork mesh for the first mesh
@@ -79,6 +80,9 @@ Mesh* booleanHelper(Mesh* inputmesh1, Mesh* inputmesh2,GLint type)
     Mesh* resultMesh = new Mesh(geometry,indices);
 
     return resultMesh;
+    */
+
+    return inputmesh1;
 }
 
 /**
